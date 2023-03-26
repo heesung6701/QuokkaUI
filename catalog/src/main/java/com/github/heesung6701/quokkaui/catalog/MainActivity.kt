82d840847074a22f9e54b22cd76d2df7e17e6917
heesung6701 <heesung6701@gmail.com>
2023-03-26 20:34:37 +0900

    Apply Compose on catalog's MainActivity

--- catalog/src/main/java/com/github/heesung6701/quokkaui/catalog/MainActivity.kt
+++ catalog/src/main/java/com/github/heesung6701/quokkaui/catalog/MainActivity.kt
@@ -1,23 +1,116 @@
-package com.github.heesung6701.quokkaui.catalog;
+package com.github.heesung6701.quokkaui.catalog
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.heesung6701.quokkaui.catalog.anchor.window.DialogAnchorActivity
+import com.github.heesung6701.quokkaui.catalog.touchdelegate.TouchDelegateActivity
 
-import android.content.Intent;
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
 
-import com.github.heesung6701.quokkaui.catalog.touchdelegate.TouchDelegateActivity;
-import com.github.heesung6701.quokkaui.catalog.anchor.window.DialogAnchorActivity;
-import com.github.heesung6701.quokkaui.catalog.databinding.ActivityMainBinding;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-public class MainActivity extends AppCompatActivity {
+        val catalogItems = listOf(
+            CatalogItemData(
+                getString(R.string.title_activity_touch_delegate),
+                TouchDelegateActivity::class.java
+            ),
+            CatalogItemData(
+                getString(R.string.title_activity_dialog_anchor),
+                DialogAnchorActivity::class.java
+            ),
+        )
+        setContent {
+            MaterialTheme {
+                Scaffold { paddingValues ->
+                    Box(modifier = Modifier.padding(paddingValues)) {
+                        CatalogItemList(
+                            titles = catalogItems.map { it.title },
+                            onItemClick = {
+                                startActivity(Intent(this@MainActivity, catalogItems[it].clazz))
+                            })
+                    }
+                }
+            }
+        }
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+    data class CatalogItemData(val title: String, val clazz: Class<out Activity>)
+}
 
-        binding.btnTouchDelegate.setOnClickListener((btn) -> startActivity(new Intent(this, TouchDelegateActivity.class)));
-        binding.btnDialogAnchor.setOnClickListener((btn) -> startActivity(new Intent(this, DialogAnchorActivity.class)));
+@Composable
+fun CatalogItemList(titles: List<String>, onItemClick: (Int) -> Unit) {
+    LazyColumn {
+        items(titles.size, key = {
+            titles[it]
+        }) {
+            CatalogItem(onClick = {
+                onItemClick(it)
+            }, title = titles[it])
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CatalogItemListPreview() {
+    MaterialTheme {
+        CatalogItemList(
+            titles = listOf("test1", "test2"),
+            onItemClick = {
+            })
+    }
+}
+
+@Composable
+fun CatalogItem(onClick: () -> Unit, title: String) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .padding(8.dp),
+        color = MaterialTheme.colorScheme.background,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                modifier = Modifier
+                    .weight(1F),
+                text = title,
+                textAlign = TextAlign.Center
+            )
+            Button(onClick = onClick) {
+                Text("go")
+            }
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun ListItemPreview() {
+    MaterialTheme {
+        CatalogItem(
+            title = "text",
+            onClick = {
+            })
     }
 }
