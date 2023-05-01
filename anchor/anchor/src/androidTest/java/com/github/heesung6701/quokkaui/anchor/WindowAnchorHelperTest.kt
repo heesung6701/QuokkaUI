dd8f05844f5a37f20b40698c8d8c7ddfafad6736
heesung6701 <heesung6701@gmail.com>
2023-05-01 15:58:52 +0900

    Add WindowAnchorHelperTest.kt

---
+++ anchor/anchor/src/androidTest/java/com/github/heesung6701/quokkaui/anchor/WindowAnchorHelperTest.kt
@@ -0,0 +1,14 @@
+package com.github.heesung6701.quokkaui.anchor
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class WindowAnchorHelperTest {
+
+    @Test
+    fun setup() {
+
+    }
+}
