8007ddba227b8845c42a2245e27018f452f1ecf7
heesung6701 <heesung6701@gmail.com>
2023-05-05 16:31:33 +0900

    Add Simple Test

--- anchor/anchor/src/androidTest/java/com/github/heesung6701/quokkaui/anchor/WindowAnchorHelperTest.kt
+++ anchor/anchor/src/androidTest/java/com/github/heesung6701/quokkaui/anchor/WindowAnchorHelperTest.kt
@@ -1,14 +1,36 @@
 package com.github.heesung6701.quokkaui.anchor
 
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class WindowAnchorHelperTest {
+    @get:Rule
+    val activityScenarioRule = activityScenarioRule<ButtonOnBorderTestActivity>()
+
+    private lateinit var activity: ButtonOnBorderTestActivity
+
+    @Before
+    fun setup() {
+        activityScenarioRule.scenario.onActivity {
+            activity = it
+        }
+    }
 
     @Test
-    fun setup() {
+    fun test_none() {
+        Assert.assertTrue(true)
+    }
 
+    @Test
+    fun testStatic() {
+        activity.buttonsList.forEach {
+            it.performClick()
+        }
     }
 }
