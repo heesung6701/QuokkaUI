97aefbd7f22df728b21ddde7d3f7adf69383e4ec
heesung6701 <heesung6701@gmail.com>
2023-05-06 16:13:49 +0900

    rename WindowAnchorConnectorTest.kt WindowAnchorHelperTest.kt

--- anchor/anchor/src/androidTest/java/com/github/heesung6701/quokkaui/anchor/WindowAnchorHelperTest.kt
+++ anchor/anchor/src/androidTest/java/com/github/heesung6701/quokkaui/anchor/WindowAnchorHelperTest.kt
@@ -18,7 +18,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class WindowAnchorConnectorTest {
+class WindowAnchorHelperTest {
     @get:Rule
     val activityScenarioRule = activityScenarioRule<ButtonOnBorderTestActivity>()
 
